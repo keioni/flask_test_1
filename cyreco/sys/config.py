32aa9428581906acac5ae2c16ff045daac4820a0
keys <keonimar@gmail.com>
2018-05-06 22:58:27 +0900

    reconfigure location of db and settings

--- cyreco/sys/config.py
+++ cyreco/sys/config.py
@@ -8,5 +8,5 @@ CONF.read('./sys/settings.cfg', 'utf-8')
 # configurations
 CONF.salt = 'oajokkN6AkwZB4wA0XpFtzlJpYYTVB7a9JkjV56PMAs'
 CONF.validation_timeout_in_sec = 3600
-CONF.db_connect_string = 'sqlite:///userlist.sqlite3'
+CONF.db_connect_string = 'sqlite:///cyreco/db/userlist.sqlite3'
 CONF.debug_sql_print = True
