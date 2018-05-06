a049fdc741e3d0de8cae878614426f33ffd8f9c7
keys <keonimar@gmail.com>
2018-05-07 06:21:41 +0900

    make configparser inherited parset

--- cyreco/sys/config.py
+++ cyreco/sys/config.py
@@ -2,11 +2,12 @@
 
 import configparser
 
-CONF = configparser.ConfigParser()
-CONF.read('./sys/settings.cfg', 'utf-8')
+class CyrecoConfigParser(configparser.ConfigParser):
+    salt = 'oajokkN6AkwZB4wA0XpFtzlJpYYTVB7a9JkjV56PMAs'
+    validation_timeout_in_sec = 3600
+    db_connect_string = 'sqlite:///cyreco/db/userlist.sqlite3'
+    debug_sql_print = True
 
-# configurations
-CONF.salt = 'oajokkN6AkwZB4wA0XpFtzlJpYYTVB7a9JkjV56PMAs'
-CONF.validation_timeout_in_sec = 3600
-CONF.db_connect_string = 'sqlite:///cyreco/db/userlist.sqlite3'
-CONF.debug_sql_print = True
+
+CONF = CyrecoConfigParser()
+CONF.read('./sys/settings.cfg', 'utf-8')
