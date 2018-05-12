b682fd5b9d9f261c1b0f91a25b335e3f227dbf78
keys <keonimar@gmail.com>
2018-05-12 19:55:38 +0900

    minimal test for ORM model.py

--- cyreco/db/model.py
+++ cyreco/db/model.py
@@ -2,7 +2,8 @@
 
 import time
 
-from sqlalchemy import Column, Integer, String, Boolean, LargeBinary
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Binary
+from datetime import datetime
 
 from cyreco.db import engine, Base
 from cyreco.utils.security import secure_hashing, generate_auth_code
@@ -10,16 +11,19 @@ from cyreco.sys.config import CONF
 
 class UsersAuthData(Base):
     __tablename__ = 'users_authdata'
-    # __table_args__ = {'mysql_engine':'InnoDB'}
+    # __table_args__ = {
+    #     'mysql_engine': 'InnoDB',
+    #     'mysql_charset': 'utf8mb4',
+    # }
 
     id = Column('id', Integer, primary_key=True, autoincrement=True)
-    name = Column('name', String(32), unique=True)
-    mailaddr = Column('mailaddr', String(254), unique=True)
-    password = Column('password', String(44))
+    name = Column('name', String(32), unique=True, nullable=False)
+    mailaddr = Column('mailaddr', String(254), unique=True, nullable=False)
+    password = Column('password', String(44), nullable=False)
     is_valid = Column('is_valid', Boolean)
     is_active = Column('is_active', Boolean)
-    create_time = Column('create_time', Integer)
-    last_login = Column('last_login', Integer)
+    create_time = Column('create_time', DateTime)
+    # last_login = Column('last_login', DateTime)
 
     def __init__(self, name: str,  plain_password: str, mailaddr: str):
         self.name = name
@@ -27,7 +31,8 @@ class UsersAuthData(Base):
         self.password = secure_hashing(plain_password, CONF.salt)
         self.is_valid = False
         self.is_active = True
-        self.create_time = self.last_login = int(time.time())
+        self.create_time = datetime.now()
+        # self.last_login = datetime.now()
 
     def __repr__(self):
         repr_args = ', '.join([
@@ -38,13 +43,17 @@ class UsersAuthData(Base):
             "is_valid='{}'".format(self.is_valid),
             "is_active='{}'".format(self.is_active),
             "create_time={}".format(self.create_time),
-            "last_login={}".format(self.last_login),
+            # "last_login={}".format(self.last_login),
         ])
         return "<UsersAuthData({})".format(repr_args)
 
 
 class UsersProfile(Base):
     __tablename__ = 'users_profile'
+    # __table_args__ = {
+    #     'mysql_engine': 'InnoDB',
+    #     'mysql_charset': 'utf8mb4',
+    # }
 
     name = Column('name', String(32), primary_key=True)
     friendly_name = Column('friendly_name', String)
@@ -61,18 +70,22 @@ class UsersProfile(Base):
 
 class UsersValidation(Base):
     __tablename__ = 'users_validation'
+    # __table_args__ = {
+    #     'mysql_engine': 'InnoDB',
+    #     'mysql_charset': 'utf8mb4',
+    # }
 
     name = Column('name', String(32), primary_key=True)
     mailaddr = Column('mailaddr', String(254), unique=True)
     auth_code = Column('auth_code', String(8))
-    expire_time = Column('expire_time', Integer)
+    expire_time = Column('expire_time', DateTime)
 
     def __init__(self, name: str, mailaddr: str, auth_code: str):
         times_to_expire = CONF.validation_timeout_in_sec
         self.name = name
         self.mailaddr = mailaddr
         self.auth_code = auth_code
-        self.expire_time = time.time() + int(times_to_expire)
+        self.expire_time = datetime.fromtimestamp(time.time() + int(times_to_expire))
 
     def __repr__(self):
         repr_args = ', '.join([
@@ -91,14 +104,17 @@ class UsersValidation(Base):
 
 class UsersRecord(Base):
     __tablename__ = 'users_record'
-    # __table_args__ = {'mysql_engine':'InnoDB'}
+    # __table_args__ = {
+    #     'mysql_engine': 'InnoDB',
+    #     'mysql_charset': 'utf8mb4',
+    # }
 
     record_id = Column('record_id', Integer, primary_key=True, autoincrement=True)
-    name = Column('name', String(32), index=True)
-    record_caption = Column('record_caption', String(64))
-    last_update = Column('last_update', Integer)
+    name = Column('name', String(32), index=True, nullable=False)
+    record_caption = Column('record_caption', String(64), nullable=False)
+    last_update = Column('last_update', DateTime, onupdate=datetime.now)
     record_count = Column('record_count', Integer)
-    record_data = Column('record_data', LargeBinary(200))
+    record_data = Column('record_data', Binary(200))
 
     def __init__(self, name: str, record_caption: str):
         self.name = name
@@ -125,7 +141,7 @@ class UsersRecordArchive(Base):
 
     record_id = Column('archive_id', Integer, primary_key=True)
     record_count = Column('record_count', Integer)
-    record_data = Column('record_data', LargeBinary(60000))
+    record_data = Column('record_data', Binary(60000))
 
     def __init__(self, name: str, record_id: int):
         self.record_id = record_id
