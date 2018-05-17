b5d15b2eed7072a0d609ea54c4e1c87a45da5b5e
keys <keonimar@gmail.com>
2018-05-17 21:55:47 +0900

    add CyRecordBox class to record.py

--- cyreco/db/record.py
+++ cyreco/db/record.py
@@ -7,6 +7,21 @@ from cyreco.utils.security import secure_hashing, generate_auth_code
 from cyreco.sys.config import CONF
 
 
+# class CyRecord:
+#     pass
+
+
+class CyRecordBox:
+    name = ''
+    records = []
+
+    def __init__(self, name: str):
+        self.name = name
+
+    def add_record(self):
+        pass
+
+
 class CyRecordManager:
 
     def create_record_box(self, box_name: str):
